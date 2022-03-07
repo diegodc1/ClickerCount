262f3255dc3d1c4a5aef3a876078df4d71f19149
Diego Alves <diegoalvesdc12@gmail.com>
2022-03-07 18:09:51 -0300

    Second Commit

--- main.js
+++ main.js
@@ -2,29 +2,38 @@ const counter = document.getElementById("counter");
 const addButton = document.querySelectorAll("addButton");
 const removeButton = document.querySelectorAll("removeButton");
 const resetButton = document.querySelectorAll("resetButton");
-const noSoundButton = document.querySelectorAll("noSoundButton");
+const muteButton = document.querySelectorAll("muteButton");
+// const muteImage = document.getElementById("muteImage");
 
-let sound = new Audio("/assets/click-sound.wav");
+let addSoundEffect = new Audio("/assets/click-sound.wav");
+let removeSoundEffect = new Audio("/assets/click-sound-remove.wav");
 let counterNumber = 0;
 
 function addNumber() {
   counterNumber++;
   counter.textContent = counterNumber;
-  sound.play();
+  addSoundEffect.play();
 }
 
 function removeNumber() {
   counterNumber--;
   counter.textContent = counterNumber;
-  sound.play();
+  removeSoundEffect.play();
 }
 
 function resetNumber() {
   counterNumber = 0;
   counter.textContent = counterNumber;
-  sound.play();
 }
 
-function noSound() {
-  sound = 0;
+function muteSound() {
+  if (addSoundEffect.muted == true) {
+    addSoundEffect.muted = false;
+    removeSoundEffect.muted = false;
+    muteImage.src = "/assets/volume-mute.svg";
+  } else {
+    addSoundEffect.muted = true;
+    removeSoundEffect.muted = true;
+    muteImage.src = "/assets/volume-muted.svg";
+  }
 }
