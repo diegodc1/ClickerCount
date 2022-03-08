44ee5db2f3a055854d27163129b774ac196be3df
Diego Alves <diegoalvesdc12@gmail.com>
2022-03-08 13:38:05 -0300

    Seconde Commit

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const addButton = document.querySelectorAll("addButton");
 const removeButton = document.querySelectorAll("removeButton");
 const resetButton = document.querySelectorAll("resetButton");
 const muteButton = document.querySelectorAll("muteButton");
-// const muteImage = document.getElementById("muteImage");
 
-let addSoundEffect = new Audio("/assets/click-sound.wav");
-let removeSoundEffect = new Audio("/assets/click-sound-remove.wav");
+const addSoundEffect = new Audio("/assets/click-sound.wav");
+const removeSoundEffect = new Audio("/assets/click-sound-remove.wav");
+const resetSound = new Audio("/assets/reset-sound.wav");
+
 let counterNumber = 0;
 
 function addNumber() {
@@ -24,16 +25,19 @@ function removeNumber() {
 function resetNumber() {
   counterNumber = 0;
   counter.textContent = counterNumber;
+  resetSound.play();
 }
 
 function muteSound() {
   if (addSoundEffect.muted == true) {
     addSoundEffect.muted = false;
     removeSoundEffect.muted = false;
+    resetSound.muted = false;
     muteImage.src = "/assets/volume-mute.svg";
   } else {
     addSoundEffect.muted = true;
     removeSoundEffect.muted = true;
+    resetSound.muted = true;
     muteImage.src = "/assets/volume-muted.svg";
   }
 }
